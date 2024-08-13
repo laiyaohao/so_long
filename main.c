52d1faeb26a4c4b3efef0b12a86e3ccddeaf6d18
Yao Hao Lai <ylai@student.42signapore.sg>
2024-08-13 20:01:34 +0800

    feat(so_long): added comments on the use of mlx_loop, mlx_pixel_put and mlx_string_put

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ylai <ylai@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/27 16:13:05 by ylai              #+#    #+#             */
-/*   Updated: 2024/08/10 18:27:03 by ylai             ###   ########.fr       */
+/*   Updated: 2024/08/13 19:49:01 by ylai             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	main(void)
 {
 	void	*mlx;
 	void	*window;
-	void	*image;
+	// void	*image;
 
 	mlx = mlx_init();
 	if (!mlx)
@@ -32,17 +32,31 @@ int	main(void)
 			fprintf(stderr, "Failed to create window\n");
 			return 1;
 	}	
+	// image = mlx_new_image(mlx, 50, 50);
+	mlx_string_put(mlx, window, 300,300, 0x00FF0000,"habfsanf");
+	// 0x00 is just the filler
+	// then, RRGGBB is the color
+	// in above example, FF is for red, 00 is for green, and 00 is for blue
+	// FF is 255, 00 is 0, and 00 is 0
+	// so above translate to 255, 0, 0 in rgb, which means red.
+	
+	/*
+	int i = 1;
+	while (i < 300)
+	{
+		mlx_pixel_put(mlx, window, i, i, 0x00FF0000);
+		i++;
+	}
 
+	*/
 	// Enter the MiniLibX loop
+	// mlx_loop waits for events like mouse clicks, key presses
+	// so after this function, i cannot draw on the window anymore
 	mlx_loop(mlx);
 
-	image = mlx_new_image(mlx, 50, 50);
-	// int i = 1;
-	// while (i < 300)
-	// {
-	// 	mlx_pixel_put(mlx, window, i, i, 3319890);
-	// 	i++;
-	// }
+	
+	
+	
 	
 	return 0;
 }
